cc422ada9f4d8c10c337c504fc651b7de08e07f5
Tongwei <2846888378@qq.com>
2022-08-24 14:19:51 +0800

    ltw

--- src/DAO/Curriculum/Course.java
+++ src/DAO/Curriculum/Course.java
@@ -1,7 +1,7 @@
 package DAO.Curriculum;
 
 public class Course {
-    int [][][]class_time=new int[17][6][14];
+    int [][][]class_time=new int[17][6][14];   //上课时间
     String name,teacher,id,classroom,timestring;
     double point;
     int size;
