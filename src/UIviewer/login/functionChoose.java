f96f71e1469db6b96af65e73d0c8e9c2166f30e5
Lenovo <gezhangyang@outlook.com>
2022-08-26 18:13:49 +0800

    gzy

--- src/UIviewer/login/functionChoose.java
+++ src/UIviewer/login/functionChoose.java
@@ -13,20 +13,24 @@ public class functionChoose {
     public static void functionChooseUI(ClientToServer ucs) {
         JFrame jf = new JFrame("functionChoose");
         jf.setSize(1273,784);
+        JPanel fc_panel = new JPanel();
+        fc_panel.setLayout(null);
+        fc_panel.setBounds(0,0,1273,784);
+        jf.setContentPane(fc_panel);
 
         //小头像
         JLabel touxiang = new JLabel();
         ImageIcon icon = new ImageIcon("src/image/头像.png");
         touxiang.setIcon(icon);
         touxiang.setBounds(20, 20, 75, 75);
-        jf.getContentPane().add(touxiang);
+        fc_panel.add(touxiang);
 
         //文字
         JLabel l1 = new JLabel("  你好！");
         l1.setBounds(100, 20, 200, 75);
         Font font = new Font("楷体", Font.BOLD, 22);
         l1.setFont(font);
-        jf.getContentPane().add(l1);
+        fc_panel.add(l1);
 
         //信息面板
         JLabel l4 = new JLabel("      基本信息");
@@ -34,7 +38,7 @@ public class functionChoose {
         Font font2 = new Font("楷体", Font.BOLD, 25);
         l4.setFont(font2);
         l4.setForeground(new Color(30,144,255));
-        jf.getContentPane().add(l4);
+        fc_panel.add(l4);
         String name=null;
         if(ucs.getID().equals("1"))
             name=ucs.getS().getStudent_name();
@@ -47,7 +51,7 @@ public class functionChoose {
         Font font1 = new Font("微软雅黑", Font.PLAIN, 18);
         l2.setFont(font1);
         l2.setForeground(new Color(0,0,0));
-        jf.getContentPane().add(l2);
+        fc_panel.add(l2);
         String card=null;
         if(ucs.getID().equals("1"))
             card=ucs.getS().getStudent_idcard();
@@ -59,27 +63,27 @@ public class functionChoose {
         l3.setBounds(30, 250, 250, 60);
         l3.setFont(font1);
         l3.setForeground(new Color(0,0,0));
-        jf.getContentPane().add(l3);
+        fc_panel.add(l3);
 
         //label背景
         JLabel l11 = new JLabel();
         ImageIcon icon4 = new ImageIcon("src/image/label2.png");
         l11.setIcon(icon4);
         l11.setBounds(15, 130, 285, 400);
-        jf.getContentPane().add(l11);
+        fc_panel.add(l11);
 
         //东南大学标志图片
         JLabel logo = new JLabel();
         ImageIcon icon1 = new ImageIcon("src/image/logo-mini.png");
         logo.setIcon(icon1);
         logo.setBounds(315, 5, 210, 65);
-        jf.getContentPane().add(logo);
+        fc_panel.add(logo);
         //右上角图标
         JLabel pic1 = new JLabel();
         ImageIcon icon2 = new ImageIcon("src/image/student.png");
         pic1.setIcon(icon2);
         pic1.setBounds(1100, 15, 25, 25);
-        jf.getContentPane().add(pic1);
+        fc_panel.add(pic1);
 
         JButton btnNewButton_6 = new JButton("安全退出");
         btnNewButton_6.setBounds(1140, 15, 100, 30);
@@ -88,7 +92,7 @@ public class functionChoose {
         btnNewButton_6.setBackground(new Color(248,248,255));
         btnNewButton_6.setContentAreaFilled(false);//设置按钮透明
         btnNewButton_6.setFocusPainted(false);
-        jf.getContentPane().add(btnNewButton_6);
+        fc_panel.add(btnNewButton_6);
 
         btnNewButton_6.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -107,20 +111,20 @@ public class functionChoose {
         ImageIcon icon5 = new ImageIcon("src/image/banner3.png");
         l12.setIcon(icon5);
         l12.setBounds(310, 70, 1000, 125);
-        jf.getContentPane().add(l12);
+        fc_panel.add(l12);
 
         //文字
         JLabel l13 = new JLabel("     功能选择 ");
         l13.setBounds(320, 200, 200, 75);
         Font font13 = new Font("微软雅黑", Font.BOLD, 25);
         l13.setFont(font13);
-        jf.getContentPane().add(l13);
+        fc_panel.add(l13);
 
         //左侧面板
         JPanel p1 = new JPanel();
         p1.setBounds(0, 0, 310, 784);
         p1.setBackground(new Color(135,206,250, 180));
-        jf.getContentPane().add(p1);
+        fc_panel.add(p1);
 
         //五个按钮
         //右下面板，学籍管理模块
@@ -128,7 +132,7 @@ public class functionChoose {
         ImageIcon icon31 = new ImageIcon("src/image/icon_72 (1).png");
         l31.setIcon(icon31);
         l31.setBounds(449, 290, 300, 72);
-        jf.getContentPane().add(l31);
+        fc_panel.add(l31);
         JButton btnNewButton_1 = new JButton("学籍管理");
         btnNewButton_1.addActionListener(new ActionListener() {
             @Override
@@ -136,20 +140,21 @@ public class functionChoose {
                 // TODO Auto-generated method stub
                 try {
                     JButton jb = new JButton();
-                    JFrame frame = new JFrame("student_status");
                     Dimension screensize=Toolkit.getDefaultToolkit().getScreenSize();
                     int width=(int ) screensize.getWidth(); //得到宽度
                     int height=(int ) screensize.getHeight();//获得高度
-                    frame.setBounds(0,0,width,height);
-                    frame.setContentPane(new student_status(ucs,width,height));
+                    jf.setBounds(0,0,width,height);
+                    jf.remove(fc_panel);
+
+                    jf.setContentPane(new student_status(ucs,width,height));
                     jb.addActionListener(new ActionListener() {
                         @Override
                         public void actionPerformed(ActionEvent e) {
-                            frame.getContentPane().setVisible(false);
+                            jf.getContentPane().setVisible(false);
                         }
                     });
-                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                    frame.setVisible(true);
+                    jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    jf.setVisible(true);
                     //返回功能选择模块
                     back_from_status.addActionListener(new ActionListener() {
                         @Override
@@ -168,17 +173,17 @@ public class functionChoose {
         btnNewButton_1.setFont(myfont);
         btnNewButton_1.setBackground(new Color(220,220,220));
         btnNewButton_1.setFocusPainted(false);
-        jf.getContentPane().add(btnNewButton_1);
+        fc_panel.add(btnNewButton_1);
         JPanel p21 = new JPanel();
         p21.setBounds(335, 290, 300, 200);
         p21.setBackground(new Color(248,248,255));
-        jf.getContentPane().add(p21);
+        fc_panel.add(p21);
 
         JLabel l32 = new JLabel();
         ImageIcon icon32 = new ImageIcon("src/image/icon_72 (3).png");
         l32.setIcon(icon32);
         l32.setBounds(754, 290, 300, 72);
-        jf.getContentPane().add(l32);
+        fc_panel.add(l32);
         JButton btnNewButton_2 = new JButton("选课系统");
         btnNewButton_2.setFocusPainted(false);
         btnNewButton_2.addActionListener(new ActionListener() {
@@ -196,17 +201,17 @@ public class functionChoose {
         btnNewButton_2.setBounds(640, 362, 300, 128);
         btnNewButton_2.setFont(myfont);
         btnNewButton_2.setBackground(new Color(220,220,220));
-        jf.getContentPane().add(btnNewButton_2);
+        fc_panel.add(btnNewButton_2);
         JPanel p22 = new JPanel();
         p22.setBounds(640, 290, 300, 200);
         p22.setBackground(new Color(248,248,255));
-        jf.getContentPane().add(p22);
+        fc_panel.add(p22);
 
         JLabel l33 = new JLabel();
         ImageIcon icon33 = new ImageIcon("src/image/icon_72 (5).png");
         l33.setIcon(icon33);
         l33.setBounds(1059, 290, 300, 72);
-        jf.getContentPane().add(l33);
+        fc_panel.add(l33);
         JButton btnNewButton_3 = new JButton("商店系统");
         btnNewButton_3.setFocusPainted(false);
         btnNewButton_3.addActionListener(new ActionListener() {
@@ -226,17 +231,17 @@ public class functionChoose {
         btnNewButton_3.setBackground(new Color(220,220,220));
         //btnNewButton_3.setBorder(null);//取消边框
         //btnNewButton_1.setForeground(new Color(248, 248, 255));
-        jf.getContentPane().add(btnNewButton_3);
+        fc_panel.add(btnNewButton_3);
         JPanel p23 = new JPanel();
         p23.setBounds(945, 290, 300, 200);
         p23.setBackground(new Color(248,248,255));
-        jf.getContentPane().add(p23);
+        fc_panel.add(p23);
 
         JLabel l34 = new JLabel();
         ImageIcon icon34 = new ImageIcon("src/image/icon_72 (4).png");
         l34.setIcon(icon34);
         l34.setBounds(449, 500, 300, 72);
-        jf.getContentPane().add(l34);
+        fc_panel.add(l34);
         JButton btnNewButton_4 = new JButton("图书馆系统");
         btnNewButton_4.setFocusPainted(false);
         btnNewButton_4.addActionListener(new ActionListener() {
@@ -262,17 +267,17 @@ public class functionChoose {
         btnNewButton_4.setBackground(new Color(220,220,220));
         //btnNewButton_4.setBorder(null);//取消边框
         //btnNewButton_1.setForeground(new Color(248, 248, 255));
-        jf.getContentPane().add(btnNewButton_4);
+        fc_panel.add(btnNewButton_4);
         JPanel p24 = new JPanel();
         p24.setBounds(335, 500, 300, 200);
         p24.setBackground(new Color(248,248,255));
-        jf.getContentPane().add(p24);
+        fc_panel.add(p24);
 
         JLabel l35 = new JLabel();
         ImageIcon icon35 = new ImageIcon("src/image/icon_72 (6).png");
         l35.setIcon(icon35);
         l35.setBounds(754, 500, 300, 72);
-        jf.getContentPane().add(l35);
+        fc_panel.add(l35);
         JButton btnNewButton_5 = new JButton("站内通信");
         btnNewButton_5.setFocusPainted(false);
         btnNewButton_5.addActionListener(new ActionListener() {
@@ -292,23 +297,23 @@ public class functionChoose {
         btnNewButton_5.setBackground(new Color(220,220,220));
         //btnNewButton_5.setBorder(null);//取消边框
         //btnNewButton_1.setForeground(new Color(248, 248, 255));
-        jf.getContentPane().add(btnNewButton_5);
+        fc_panel.add(btnNewButton_5);
         JPanel p25 = new JPanel();
         p25.setBounds(640, 500, 300, 200);
         p25.setBackground(new Color(248,248,255));
-        jf.getContentPane().add(p25);
+        fc_panel.add(p25);
 
         //右下面板
         JPanel p3 = new JPanel();
         p3.setBounds(330, 285, 920, 420);
         p3.setBackground(new Color(211,211,211,100));
-        jf.getContentPane().add(p3);
+        fc_panel.add(p3);
 
         //右侧面板
         JPanel p2 = new JPanel();
         p2.setBounds(300, 0, 950, 784);
         p2.setBackground(new Color(245,245,245, 180));
-        jf.getContentPane().add(p2);
+        fc_panel.add(p2);
         jf.setLocationRelativeTo(null);
         jf.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         jf.setVisible(true);
