d91d328190ea93751d3474916173de826bbf28a6
chen-guanzhi <1324216068@qq.com>
2022-08-28 12:40:15 +0800

    cgz

--- src/UIviewer/Library/myBook.java
+++ src/UIviewer/Library/myBook.java
@@ -6,6 +6,8 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class myBook extends JPanel {
     public volatile String[][] myBook=null;
@@ -29,12 +31,47 @@ public class myBook extends JPanel {
         //数据
         DefaultTableModel dtm = new DefaultTableModel(Client_library.ret_my_books, tableTitle);
         JTable table_want = new JTable(dtm);
+        table_want.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                    if(table_want.getSelectedColumn()==7){
+                        System.out.println(table_want.getSelectedRow());
+                    }
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+
         //支持滚动
         JScrollPane jsp = new JScrollPane(table_want);
         jsp.setBounds(0,0,1280,680);
         add(jsp);
+
+        public boolean isCellEditable(int row, int column)
+        {
+            return false;
+        }
+
         table_want.setRowHeight(30);
-        table_want.getColumnModel().getColumn(7).setCellRenderer(new MyButtonRender("10010"));
-        table_want.getColumnModel().getColumn(8).setCellRenderer(new MyButtonRender1());
+        //table_want.getColumnModel().getColumn(7).setCellRenderer(new MyButtonRender("10010"));
+        //table_want.getColumnModel().getColumn(8).setCellRenderer(new MyButtonRender1());
     }
 }
