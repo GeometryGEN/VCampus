a2e87cfc15075fbe1ddf35f99e496ff0f36b1ac8
chen-guanzhi <1324216068@qq.com>
2022-08-29 12:33:23 +0800

    cgz

--- src/ClientToServer/ClientToServerThread.java
+++ src/ClientToServer/ClientToServerThread.java
@@ -77,7 +77,7 @@ public class ClientToServerThread extends Thread {
                     Client_library.showSearchResult(searchResult);
                 }
                 else if(message.getType().equals(MessageType.MESSAGE_LIBRARY_RET_SUCCEED)){
-                    //JOptionPane.showMessageDialog(null,"还书成功!");
+                    JOptionPane.showMessageDialog(null,"还书成功!");
                 }
                 else if(message.getType().equals(MessageType.MESSAGE_LIBRARY_RET_LATE)){
                     JOptionPane.showMessageDialog(null,"还书迟了，请记得按时还书!");
