09114d3f1d9cbe3cdbf94d479da7778b047a5b34
chen-guanzhi <1324216068@qq.com>
2022-08-30 02:14:01 +0800

    cgz

--- src/UIviewer/Library/readLib.java
+++ src/UIviewer/Library/readLib.java
@@ -5,9 +5,17 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.IOException;
 import UIviewer.login.functionChoose;
+import net.coobird.thumbnailator.Thumbnails;
+
 public class readLib extends JPanel {
+    Dimension screensize=Toolkit.getDefaultToolkit().getScreenSize();
+    int width=(int ) screensize.getWidth(); //得到宽度
+    int height=(int ) screensize.getHeight();//获得高度
+    double width_r=(double)(width)/1273;
+    double height_r=(double)(height)/784;
     public static JPanel panel = new JPanel();;
     static String name;
     public static JButton b1=new JButton("馆藏查询");
@@ -30,10 +38,10 @@ public class readLib extends JPanel {
             else if(ucs.getID().equals("3"))
                 name=ucs.getA().getAdmin_name();
             getName(name);
-        setBounds(0,0,1280,790);
+        setBounds(0,0, (int) (1280*width_r), (int) (790*height_r));
         setLayout(null);
 
-        panel.setBounds(0, 150, 1273, 790);
+        panel.setBounds(0, (int) (150*height_r), (int) (1280*width_r), (int) (790*height_r));
         add(panel);
 //		给主要显示面板添加布局方式
         panel.setLayout(cardLayout);
@@ -47,28 +55,37 @@ public class readLib extends JPanel {
         //图书馆标志与背景
         JLabel logo = new JLabel();
         ImageIcon icon = new ImageIcon("src/image/logonew.png");
-        logo.setIcon(icon);
-        logo.setBounds(30, 10, 600, 75);
+            int icon1_width= 600;
+            int icon1_height=75;
+            try {
+                Thumbnails.of(new File("src/image/logonew.png"))
+                        .size((int)(icon1_width*width_r), (int)(icon1_height*height_r))
+                        .toFile(new File("src/image/logonew_min.png"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            logo.setIcon(new ImageIcon("src/image/logonew_min.png"));
+        logo.setBounds((int) (30*width_r), (int) (10*height_r), (int) (600*width_r), (int) (75*height_r));
         add(logo);
 
         //文字
             JLabel l1 = new JLabel("你好！"+name);
-            l1.setBounds(1100, 30, 200, 55);
+            l1.setBounds((int) (1100*width_r), (int) (30*height_r), (int) (200*width_r), (int) (55*height_r));
             l1.setForeground(new Color(248, 248, 255));
-            Font font = new Font("楷体", Font.BOLD, 20);
+            Font font = new Font("楷体", Font.BOLD, (int) (20*width_r));
             l1.setFont(font);
             add(l1);
 
         //上方面板
         JPanel p1 = new JPanel();
-        p1.setBounds(0, 0, 1280, 100);
+        p1.setBounds(0, 0, (int) (1280*width_r), (int) (100*height_r));
         p1.setBackground(new Color(5,44,5));
         add(p1);
 
         //按钮
 
-        b1.setBounds(100,100,250,50);
-        Font myfont1 = new Font("微软雅黑", Font.BOLD, 18);
+        b1.setBounds((int) (100*width_r), (int) (100*height_r), (int) (250*width_r), (int) (50*height_r));
+        Font myfont1 = new Font("微软雅黑", Font.BOLD, (int) (18*width_r));
         b1.setFont(myfont1);
         b1.setContentAreaFilled(false);//设置按钮透明
         b1.setFocusPainted(false);
@@ -83,7 +100,7 @@ public class readLib extends JPanel {
         add(b1);
 
 
-        b2.setBounds(370,100,250,50);
+        b2.setBounds((int) (370*width_r), (int) (100*height_r), (int) (250*width_r), (int) (50*height_r));
         b2.setFont(myfont1);
         b2.setContentAreaFilled(false);//设置按钮透明
         b2.setFocusPainted(false);
@@ -101,7 +118,7 @@ public class readLib extends JPanel {
         });
         add(b2);
 
-        b3.setBounds(640,100,250,50);
+        b3.setBounds((int) (640*width_r), (int) (100*height_r), (int) (250*width_r), (int) (50*height_r));
         b3.setFont(myfont1);
         b3.setContentAreaFilled(false);//设置按钮透明
         b3.setFocusPainted(false);
@@ -121,7 +138,7 @@ public class readLib extends JPanel {
         add(b3);
 
         JButton b4=new JButton("退出图书馆");
-        b4.setBounds(910,100,250,50);
+        b4.setBounds((int) (910*width_r), (int) (100*height_r), (int) (250*width_r), (int) (50*height_r));
         b4.setFont(myfont1);
         b4.setContentAreaFilled(false);//设置按钮透明
         b4.setFocusPainted(false);
@@ -138,7 +155,7 @@ public class readLib extends JPanel {
 
         //按钮面板
         JPanel p2 = new JPanel();
-        p2.setBounds(0, 100, 1280, 50);
+        p2.setBounds(0, (int) (100*height_r), (int) (1280*width_r), (int) (50*height_r));
         p2.setBackground(new Color(57,106,57));
         add(p2);
 
