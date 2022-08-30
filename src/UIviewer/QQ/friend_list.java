e4092616bc886880760ceb1be884d9e05483ae6a
Lenovo <gezhangyang@outlook.com>
2022-08-30 10:24:16 +0800

    gzy

--- src/UIviewer/QQ/friend_list.java
+++ src/UIviewer/QQ/friend_list.java
@@ -16,7 +16,7 @@ import javax.swing.*;
 
 public class friend_list extends JPanel{
 
-    public static JPanel roll_panel=new JPanel();
+    public static JPanel roll_panel/*=new JPanel()*/;
     private static int width;
 
     static int number_per_page=7;//一页几个好友,即好友条长度
@@ -26,7 +26,7 @@ public class friend_list extends JPanel{
    // private static int[] clickF=new int[50];
     private static Friend[] friendList= new Friend[50];
     private static String[] tagList= new String[50];
-    private static int count=1;
+    private static int count;
     public void update(){//更新UI界面；
         this.updateUI();
     }
@@ -35,14 +35,13 @@ public class friend_list extends JPanel{
         int num=arrayList.size();
         roll_panel.add(tag1);
         friend_slice[] slice= new friend_slice[10];
-        System.out.println(num);
+        count=1;
+
         for(int i=0;i<num;i++){
             friendList[count]=arrayList.get(i);
             tagList[count]=tag;
             count++;
             slice[i]=new friend_slice(width-1,height/number_per_page,width_r,height_r,arrayList.get(i).getName());
-            System.out.println(i);
-                System.out.println(arrayList.get(i).getName());
             roll_panel.add(slice[i]);
             roll_panel.updateUI();
         }
@@ -66,9 +65,7 @@ public class friend_list extends JPanel{
     }
 
    friend_list(/*ClientToServer ucs,*/int width, int height,double width_r,double height_r,int x,int y){
-
-
-
+        roll_panel=new JPanel();
         this.width=width;
         this.height=height;
         this.width_r=width_r;
@@ -87,9 +84,9 @@ public class friend_list extends JPanel{
            throw new RuntimeException(e);
        }
        JScrollPane scrollPane=new JScrollPane(roll_panel);
-        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER );//不显示水平滚动条；
-        scrollPane.setBorder(BorderFactory.createLineBorder(new Color(244,244,244)));
-        add(scrollPane, BorderLayout.CENTER);
+       scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER );//不显示水平滚动条；
+       scrollPane.setBorder(BorderFactory.createLineBorder(new Color(244,244,244)));
+       add(scrollPane, BorderLayout.CENTER);
 
    }
 }
