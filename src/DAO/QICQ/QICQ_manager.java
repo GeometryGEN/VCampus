b5ddd07538af2492c420c05698f0813598e3b598
DELL <110@120>
2022-08-31 22:07:18 +0800

    harold

--- src/DAO/QICQ/QICQ_manager.java
+++ src/DAO/QICQ/QICQ_manager.java
@@ -137,6 +137,7 @@ public class QICQ_manager {
     }
     public void add_friend(Message message) throws IOException, SQLException {
         Application app=(Application)message.getData();
+        int flag=0;
         String sql="select * from students where Student_idcard=?;";
         PreparedStatement st=conn.prepareStatement(sql);
         st.setString(1,app.to_id);
@@ -164,6 +165,7 @@ public class QICQ_manager {
 
             }
             else {
+                flag=1;
                 Message msg=new Message();
                 msg.setType(MessageType.MESSAGE_QICQ_ADD_FRIEND_FAIL_CANNOT_FIND_USER);
         //        MyObjectOutputStream oos=new MyObjectOutputStream(ManageServerToClientThread.getThread(id).getSocket().getOutputStream());
@@ -171,9 +173,12 @@ public class QICQ_manager {
             }
 
         }
-        Message msg=new Message();
-        msg.setType(MessageType.MESSAGE_QICQ_ADD_FRIEND_SUCCEED);
-        ManageServerToClientThread.getThread(id).oos.writeObject(msg);
+        if(flag==0){
+            Message msg=new Message();
+            msg.setType(MessageType.MESSAGE_QICQ_ADD_FRIEND_SUCCEED);
+            ManageServerToClientThread.getThread(id).oos.writeObject(msg);
+        }
+
     }
     public Message list_my_application() throws SQLException, IOException {
         ArrayList<Application>app=new ArrayList<>();
