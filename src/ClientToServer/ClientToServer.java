92dc74209b0c5203d5c3bdd3955afd7736dc81ff
Lenovo <gezhangyang@outlook.com>
2022-09-02 02:09:07 +0800

    gzy

--- src/ClientToServer/ClientToServer.java
+++ src/ClientToServer/ClientToServer.java
@@ -33,8 +33,8 @@ public class ClientToServer {
     public static MyObjectInputStream ois=null;
     //public static String serverIP = Message.returnIP();
     //public static String serverIP = "10.210.97.60";
-    public static String serverIP = Message.returnIP();
- //   public static String serverIP = "192.168.76.172";
+   // public static String serverIP = Message.returnIP();
+    public static String serverIP = "10.210.97.60";
 
     public String getIDcard() {
         return switch (ID) {
