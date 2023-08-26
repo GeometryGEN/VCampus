89776235f2298399d625fc50c501d1bcc6b843da
Fox_0527 <614690785@qq.com>
2023-08-26 21:49:31 +0800

    try commit

--- src/UIviewer/login/LoginFrame.java
+++ src/UIviewer/login/LoginFrame.java
@@ -32,7 +32,7 @@ public class LoginFrame extends JFrame{
         //随机背景图片
         JLabel lblBackground = new JLabel(); // 创建一个标签组件对象
         //URL resource = this.getClass().getResource("/back1.png"); // 获取背景图片路径
-        ImageIcon icon = new ImageIcon("src/image/background_test.jpg"); // 创建背景图片对象
+        ImageIcon icon = new ImageIcon("src/image/Background/bg01.jpg"); // 创建背景图片对象
         lblBackground.setIcon(icon); // 设置标签组件要显示的图标
         lblBackground.setBounds(0, 0, icon.getIconWidth(), icon.getIconHeight()); // 设置组件的显示位置及大小
 
