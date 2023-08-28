997b41d754c2e20738474c817983ae585354afd7
Fox_0527 <614690785@qq.com>
2023-08-28 15:47:29 +0800

    harold

---
+++ src/UIviewer/login/register2.java
@@ -0,0 +1,15 @@
+package UIviewer.login;
+import ClientToServer.ClientToServer;
+import User.Student;
+import User.Teacher;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class register2 {
+    public static void registerUI2(){
+
+    }
+}
