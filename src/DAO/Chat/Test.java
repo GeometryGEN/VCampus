3b665928df77a15aa67652024b8181d4346fcb5f
taohongyu0 <taohongyu333@126.com>
2023-09-07 20:39:10 +0800

    thy commit

--- src/DAO/Chat/Test.java
+++ src/DAO/Chat/Test.java
@@ -11,16 +11,15 @@ public class Test {
         CloseableHttpClient httpClient = HttpClients.createDefault();
         String apiKey = "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";
         CustomChatGpt customChatGpt = new CustomChatGpt(apiKey);
-        // 根据自己的网络设置吧
         customChatGpt.setResponseTimeout(200000);
         while (true) {
-            System.out.print("\n请输入问题(q退出)：");
+            System.out.print("\n你有什么想问我的嘛？如果想退出系统，请输入‘q’哦！^_^");
             String question = new Scanner(System.in).nextLine();
             if ("q".equals(question)) break;
             long start = System.currentTimeMillis();
             String answer = customChatGpt.getAnswer(httpClient, question);
             long end = System.currentTimeMillis();
-            System.out.println("该回答花费时间为：" + (end - start) / 1000.0 + "秒");
+            System.out.println("本次回答花费：" + (end - start) / 1000.0 + "秒。虽然有点慢，但我尽力了呢~ ^.^");
             System.out.println(answer);
         }
         httpClient.close();
