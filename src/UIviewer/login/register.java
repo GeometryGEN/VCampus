45316bf67a0b0e6750da9c20e38746d9fb963442
Fox <614690785@qq.com>
2023-09-08 10:27:49 +0800

    try

--- src/UIviewer/login/register.java
+++ src/UIviewer/login/register.java
@@ -4,12 +4,15 @@ import DAO.Login.Mail;
 import UIviewer.Library.readLib;
 import User.Student;
 import User.Teacher;
+import net.coobird.thumbnailator.Thumbnails;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Objects;
 
@@ -28,12 +31,12 @@ public class register {
     public static String captcha;//验证码
     public static String idcard;//一卡通
 
-    Color color1=new Color(31,66,71);
-    Color color2=new Color(125,182,191);
-    Color color3=new Color(111,150,134);
-    Color color4=new Color(207,219,212);
-    Font myfont1=new Font("等线", Font.BOLD, 17);
-    Font myfont2=new Font("等线", Font.BOLD, 15);
+    static Color color1=new Color(31,66,71);
+    static Color color2=new Color(125,182,191);
+    static Color color3=new Color(111,150,134);
+    static Color color4=new Color(207,219,212);
+    static Font myfont1=new Font("等线", Font.BOLD, 17);
+    static Font myfont2=new Font("等线", Font.BOLD, 15);
     /**
      * 注册界面
      */
@@ -42,44 +45,47 @@ public class register {
 
         //注册信息
         JLabel l2=new JLabel("注册邮箱");
-        l2.setBounds(300,50,290,80);
-        Font font=new Font("等线",Font.BOLD,26);
+        l2.setBounds(140,210,290,80);
+        Font font=new Font("宋体",Font.BOLD,26);
         l2.setFont(font);
-        //l1.setForeground(new Color(111,222,0));
+        l2.setForeground(color1);
         jf.getContentPane().add(l2);
 
         //账号密码
         JLabel l3=new JLabel("一卡通:");
         l3.setFont(new Font("等线",Font.BOLD,18));
-        l3.setBounds(260,130,250,25);
+        l3.setForeground(color1);
+        l3.setBounds(100,330,250,25);
         jf.add(l3);
         JTextField textField3=new JTextField();
         textField3.setFont(new Font("宋体",Font.BOLD,17));
-        textField3.setBounds(325,130,120,25);
+        textField3.setBounds(170,330,130,25);
         jf.add(textField3);
         textField3.setColumns(10);
 
         JLabel l10=new JLabel("邮箱:");
         l10.setFont(new Font("等线",Font.BOLD,18));
-        l10.setBounds(260,180,250,25);
+        l10.setForeground(color1);
+        l10.setBounds(100,380,250,25);
         jf.add(l10);
         JTextField textField10=new JTextField();
         textField10.setFont(new Font("宋体",Font.BOLD,15));
-        textField10.setBounds(325,180,120,25);
+        textField10.setBounds(170,380,130,25);
         jf.add(textField10);
         textField10.setColumns(10);
 
         //填写验证码的地方
         JTextField textField11=new JTextField();
         textField11.setFont(new Font("宋体",Font.BOLD,17));
-        textField11.setBounds(260,230,70,25);
+        textField11.setBounds(100,430,80,25);
         jf.add(textField11);
-        textField10.setColumns(11);
+        textField10.setColumns(10);
         //发送验证码
         JButton b2=new JButton("发送验证码");
         b2.setFont(new Font("等线",Font.BOLD,15));
-        b2.setBounds(336,230,110,25);
-        b2.setBackground(new Color(235,236,240));
+        b2.setBounds(190,430,110,25);
+        b2.setForeground(color1);
+        b2.setBackground(color4);
         b2.setFocusPainted(false);
         jf.add(b2);
         b2.addActionListener(new ActionListener(){
@@ -107,8 +113,10 @@ public class register {
         });
 
         JButton b1=new JButton("确认");
-        b1.setBounds(360,290,100,30);
-        b1.setBackground(new Color(235,236,240));
+        b1.setBounds(145,490,100,30);
+        b1.setBackground(color3);
+        b1.setForeground(Color.white);
+        b1.setFont(new Font("楷体", Font.PLAIN, 25));
         b1.setFocusPainted(false);
 
         //点击确认之后对比输入的验证码是否正确，若正确调用注册的函数
@@ -119,8 +127,8 @@ public class register {
                 //测试中先直接点进去，不用验证
                 //jf.setVisible(false);
                 //registerUI2(textField10.getText(),textField3.getText());
-                if(true)
-                //if(Objects.equals(mycaptcha, captcha))
+                //if(true)
+                if(Objects.equals(mycaptcha, captcha))
                 {
                     //继续注册
                     jf.setVisible(false);
@@ -136,21 +144,40 @@ public class register {
                     //报填写错误，验证码不对
                     JOptionPane.showMessageDialog(jf, "验证码错误，请选择重新发送!");
                 }
-
-
             }
         });
         jf.add(b1);
 
-
-        //背景图片s
+        JPanel p11=new JPanel();
+        p11.setBounds(0,0, 400, 200);
+        JLabel pic1 = new JLabel();
+        int icon1_width= 400;
+        int icon1_height=200;
+        //裁减2到min的尺寸
+        try {
+            Thumbnails.of(new File("src/image/登录/13.jpg"))
+                    .size((int)(icon1_width), (int)(icon1_height))
+                    .keepAspectRatio(false)
+                    .toFile(new File("src/image/登录/13_fit.jpg"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        pic1.setIcon(new ImageIcon("src/image/登录/13_fit.jpg"));
+        pic1.setBounds(0,-15 ,400,200);
+        p11.add(pic1);
+        p11.setBackground(color4);
+        jf.add(p11);
+        /*
+        //背景图片
         JLabel lblBackground=new JLabel(); // 创建一个标签组件对象
         ImageIcon icon=new ImageIcon("src/image/登录/04.png"); // 创建背景图片对象
         lblBackground.setIcon(icon); // 设置标签组件要显示的图标
         lblBackground.setBounds(50,0,icon.getIconWidth(),icon.getIconHeight()); // 设置组件的显示位置及大小
         jf.getContentPane().add(lblBackground);
 
-        jf.setBounds(0,0,690,620);
+         */
+        jf.setBackground(color4);
+        jf.setBounds(0,0,400,650);
         jf.setLocationRelativeTo(null);
         jf.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         jf.setVisible(true);
