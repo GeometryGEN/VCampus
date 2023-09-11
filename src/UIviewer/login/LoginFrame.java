ea661341c4c974793e8bdf52b9cdd74958b3cc4a
Fox <614690785@qq.com>
2023-09-11 13:48:19 +0800

    try

--- src/UIviewer/login/LoginFrame.java
+++ src/UIviewer/login/LoginFrame.java
@@ -48,7 +48,7 @@ public class LoginFrame extends JFrame{
      * 登录框
      */
     public LoginFrame(){
-        //菜单栏
+        //菜单栏aq
         JMenuBar menuBar=new JMenuBar();
         JMenu menu1=new JMenu("注册");
         menu1.setFont(myfont1);
